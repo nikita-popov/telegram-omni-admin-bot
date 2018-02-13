bd8e9a1719b634fe6899c35f9eb634bbba963333
Никита Попов <nikitasp1992@gmail.com>
2018-02-13 15:54:21 +0600

    1.2.1 Now it work. Implemented queries on the promises. Up data security.

---
+++ middleware/network.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fetch = require('node-fetch');
+const moment = require('moment');
+
+// Get current IP address /////////////////////////////////////////////////////////////////////////
+let updateIP = () => {
+  const options = {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'GET'
+  };
+  return fetch('http://ip-api.com/json', options)
+    .then((res) => {
+      console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), 'Request succes');
+      return res.json();
+    })
+    .then((data) => {
+      console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Current ip: ${data.query.toString()}`);
+      return(data.query.toString());
+    })
+    .catch((err) => {
+      console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Fetch error: ${err.message}`);
+    });
+}
+
+module.exports = { updateIP };
---
+++ test/unit/network.unit.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const omni = require('../../omni');
+const chai = require('chai');
+const assert = chai.assert;
+
+describe ('Omni module', () => {
+  describe ('updateIP()', () => {
+    context('must update IP address', () => {
+      it('should return 12', () => {
+//  (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}
+        assert.equal(12, math.sum(8, 4));
+      });
+    });
+  });
+});
