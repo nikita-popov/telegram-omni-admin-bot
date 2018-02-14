5e5f3d43be0c6c7a3814912d2ee968ac7783c9a5
Никита Попов <nikitasp1992@gmail.com>
2018-02-14 14:21:14 +0600

    1.2.1.2 Added verification of user ID

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # omni
-ddns and operation server
+DDNS and server operation bot.
+
+### h3 Available commands:
+_/getip_ - return external ip;
+
+### h3 private.js example
+    Dont't forget to edit the file with your settings
+```html
+'use strict';
+// Your bot token
+const token = '123456789:ABCDEFGHIJKLMNOPQRSTUV_123WXYZAB-CDEF';
+// Your ID
+const ownerID = '111111111';
+
+module.exports = { token };
+```
--- test/unit/network.unit.test.js
+++ test/unit/network.unit.test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const omni = require('../../omni');
+const network = require('../../middleware/network');
 const chai = require('chai');
 const assert = chai.assert;
 
-describe ('Omni module', () => {
+describe ('network module', () => {
   describe ('updateIP()', () => {
     context('must update IP address', () => {
       it('should return 12', () => {
 //  (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}
-        assert.equal(12, math.sum(8, 4));
+        assert.equal(, network.updateIP());
       });
     });
   });
