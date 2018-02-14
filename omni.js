060a221639e55fcf9c5eb42ac9e8b5e28e5a02da
Никита Попов <nikitasp1992@gmail.com>
2018-02-14 16:13:34 +0600

    1.2.1.4 Added display of the hostname in the log messages

--- omni.js
+++ omni.js
@@ -3,43 +3,45 @@
 const network = require('./middleware/network'),
       private_info = require('./private_info');
 
-const moment = require('moment');
+const moment = require('moment'),
+      fs = require('fs');
 
 const TelegramBot = require('node-telegram-bot-api'),
       omni = new TelegramBot(private_info.token, { polling: true });
 
 // BOT function ///////////////////////////////////////////////////////////////////////////////////
 omni.onText(/\/getip/, (msg) => {
+  let hostname = network.getHostname();
   if (msg.from.id != private_info.ownerID){
-    console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Unauthorized request from user ID${msg.from.id}!!!`);
-    omni.sendMessage(private_info.ownerID, `Unauthorized request from user ID${msg.from.id}!!!`)
+    console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `${hostname} Unauthorized request from user ID${msg.from.id}!!!`);
+    omni.sendMessage(private_info.ownerID, `${hostname} Unauthorized request from user ID${msg.from.id}!!!`)
       .catch((err) => {
-        console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Error: ${err.message}`);
+        console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `${hostname} Error: ${err.message}`);
       });
     omni.sendMessage(msg.chat.id, `Access denied!!!`)
       .catch((err) => {
-        console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Error: ${err.message}`);
+        console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `${hostname} Error: ${err.message}`);
       });
   } else {
     network.updateIP()
       .then(ip => {
-        console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Send ip to user: ${msg.from.id}, chat ID: ${msg.chat.id}`);
+        console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `${hostname} Send ip to user: ${msg.from.id}, chat ID: ${msg.chat.id}`);
         omni.sendMessage(msg.chat.id, `Current ip is ${ip}.`)
           .catch((err) => {
-            console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Error: ${err.message}`);
+            console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `${hostname} Error: ${err.message}`);
           });
       })
       .catch((err) => {
-        console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `UpdateIP error: ${err.message}`);
+        console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `${hostname} UpdateIP error: ${err.message}`);
       });
   }
 });
 
 // Error handling /////////////////////////////////////////////////////////////////////////////////
 omni.on('polling_error', (err) => {
-  console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Polling error: ${err.message}`);
+  console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `${hostname} Polling error: ${err.message}`);
 });
 
 omni.on('webhook_error', (err) => {
-  console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `Webhook error: ${err.message}`);
+  console.log(moment().format('YYYY-MM-DD  HH:mm:ss'), `${hostname} Webhook error: ${err.message}`);
 });
